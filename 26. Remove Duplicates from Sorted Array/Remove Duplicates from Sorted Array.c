4f542b7dd639b64d134cfa4d21488aea08752b58
e007926 <e007926@gmail.com>
2018-09-20 00:49:33 +0800

    Update Remove Duplicates from Sorted Array.c

--- 26. Remove Duplicates from Sorted Array/Remove Duplicates from Sorted Array.c
+++ 26. Remove Duplicates from Sorted Array/Remove Duplicates from Sorted Array.c
@@ -5,7 +5,7 @@ int removeDuplicates(int* nums, int numsSize) {
     //set two pointer to traverse the array
     int i = 0; //slow pointer
     for(int j = 1; j < numsSize; j++){ //j fast pointer, increase j to skip duplicate
-        if(nums[i] != nums[j]){ // if not the same, copy the value of j to i, increase i
+        if(nums[i] != nums[j]){ // if not the same, increase i and copy the value of j to i, 
             i++;
             nums[i] = nums[j];
         }
