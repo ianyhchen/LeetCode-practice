62e414cae8bac2904b744092248dace427c752be
e007926 <e007926@gmail.com>
2018-10-03 00:29:47 +0800

    Update Merge Two Binary Trees.c

--- 614.Merge Two Binary Trees/Merge Two Binary Trees.c
+++ 614.Merge Two Binary Trees/Merge Two Binary Trees.c
@@ -15,10 +15,11 @@ struct TreeNode* mergeTrees(struct TreeNode* t1, struct TreeNode* t2) {
     if(!t2){
         return t1;
     }
-    struct TreeNode *tree = (struct TreeNode*)malloc(sizeof(struct TreeNode));
-    tree->val = t1->val + t2->val;
-    tree->left = mergeTrees(t1->left, t2->left);
-    tree->right = mergeTrees(t1->right, t2->right);
-    return tree;
+    // preorder traversal
+    //struct TreeNode *tree = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    t1->val = t1->val + t2->val;
+    t1->left = mergeTrees(t1->left, t2->left);
+    t1->right = mergeTrees(t1->right, t2->right);
+    return t1;
     
 }
