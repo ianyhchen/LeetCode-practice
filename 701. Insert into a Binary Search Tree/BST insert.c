7a527358c8167c7065b653494654affb9b406948
e007926 <e007926@gmail.com>
2018-10-05 11:25:02 +0800

    Create BST insert.c

---
+++ 701. Insert into a Binary Search Tree/BST insert.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* newNode(int val){
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    if(root == NULL){
+        return newNode(val);
+    }
+    
+    if(val < root->val){        
+        root->left = insertIntoBST(root->left, val);
+        
+    }
+    if(val > root->val){
+        root->right = insertIntoBST(root->right, val);        
+    }
+    return root;
+}
